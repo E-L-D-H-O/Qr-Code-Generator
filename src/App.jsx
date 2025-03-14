28bd15e6479768757f3cfbda2b774692d9ac76ef
ELDHO SHAJU <eldhoshaju1988@gmail.com>
2025-03-14 10:27:39 -0400

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import Navbar from "./assets/components/Navbar";
-import QRGenerator from "./assets/components/QRGenerator";
-import Footer from "./assets/components/Footer";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RENDER_PATH } from "./common/constants";
+import LoginContainer from "./components/loginContainer";
+import SignupContainer from "./components/SignUpContainer";
+import LandingContainer from "./components/landingContainer";
+import QRGenerator from "./components/QRGenerator";
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <QRGenerator />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path={RENDER_PATH.LOGIN_URL} element={<LoginContainer />} />
+      </Routes>
+      <Routes>
+        <Route exact path={RENDER_PATH.SIGN_UP_URL} element={<SignupContainer />} />
+      </Routes>
+      <Routes>
+        <Route exact path={RENDER_PATH.LANDING_URL} element={<LandingContainer />} />
+      </Routes>
+      <Routes>
+        <Route exact path={RENDER_PATH.CREATE_QR_URL} element={<QRGenerator />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/common/constants.js
@@ -0,0 +1,8 @@
+export const RENDER_PATH = {
+    LANDING_URL: "/",
+    LOGIN_URL: "/login",
+    SIGN_UP_URL: "/signup",
+    CREATE_QR_URL: "/createqr",
+    MY_ACCOUNT_URL: "/dashboard",
+    SUCCESS: "/success"
+}
