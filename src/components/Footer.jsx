1ea56faba2cb26486c479ccecf1bdbb20ddeddad
Manav Modi <manavmodi20@gmail.com>
2025-04-15 11:36:59 -0400

    Footer UI Fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-primary text-white text-center py-3 mt-auto fixed-bottom">
+    <footer className="bg-primary text-white text-center py-3 mt-4">
       © 2025 QR Generator | All Rights Reserved
     </footer>
   );
