32c79ece2ba8d9fd5c91a8cb0aa8fee78bac25db
ELDHO SHAJU <eldhoshaju1988@gmail.com>
2025-04-15 12:31:23 -0400

    Fix rollup build error by regenerating lockfile

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+process.env.ROLLUP_FORCE_JS = 'true';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
-
-
-
+});
